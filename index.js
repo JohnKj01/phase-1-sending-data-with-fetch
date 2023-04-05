65a0a11370175cf824a779b0063ee562826efa3b
John <johnkaranjairungu@gmail.com>
2023-04-05 15:12:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your code here
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    dogName: "john",
+    dogBreed: "johnkaranja01@moringaschool.com",
+  }),
+});
